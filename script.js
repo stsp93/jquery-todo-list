047989797d4d5357b4170ebcdd141864fe3d3989
stsp93 <stspsp@gmail.com>
2023-07-31 16:28:22 +0300

    added delete btn

--- script.js
+++ script.js
@@ -32,12 +32,29 @@ addBtn.click(function () {
     allTasks = localStorage.getItem('tasks');
 });
 
-// delete button
+
+listEl.click(function(ev) {
+    const target = ev.target
+    const taskId = target.parentElement.parentElement.dataset.id;
+    // delete button
+    if(target.classList.contains('btn-danger')) {
+
+        // remove form localStorage
+        const taskArray = JSON.parse(allTasks);
+        taskArray.splice(taskId, 1);
+        localStorage.setItem('tasks', JSON.stringify(taskArray));
+
+        // delete Element
+        target.parentElement.parentElement.remove()
+    };
+    
+
+})
 
 
 function createMarkup(task, id) {
     return `<li data-id=${id} class="list-group-item d-flex justify-content-between align-items-center">
-    ${id}. ${task}
+    ${id+1}. ${task}
     <div>
       <button type="button" class="btn btn-warning mr-2">Edit</button>
       <button type="button" class="btn btn-danger">Delete</button>
