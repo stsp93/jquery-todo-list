f82a68382568587208bad1a366eb65f9a8fddb6c
stsp93 <stspsp@gmail.com>
2023-07-31 16:07:25 +0300

    persist tasks in local storage

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 const addBtn = $('#btn-add');
 const listEl = $('.list-group');
 
+let allTasks = localStorage.getItem('tasks');
+
+// inital load
+
+if(allTasks) {
+    JSON.parse(allTasks).forEach(t => {
+        listEl.append(createMarkup(t));
+    })
+}
+
+
+// add button
+
 addBtn.click(function () {
     const task = $('#taskInput').val();
 
-    const markup = `<li class="list-group-item d-flex justify-content-between align-items-center">
+    console.log(task);
+
+    listEl.append(createMarkup(task));
+
+    $('#taskInput').val('');
+    
+    if(!allTasks) {
+        localStorage.setItem('tasks',JSON.stringify([task]));
+    }else {
+        localStorage.setItem('tasks',JSON.stringify([...JSON.parse(allTasks), task]));
+    }
+
+    allTasks = localStorage.getItem('tasks');
+});
+
+function createMarkup(task) {
+    return `<li class="list-group-item d-flex justify-content-between align-items-center">
     ${task}
     <div>
       <button type="button" class="btn btn-warning mr-2">Edit</button>
       <button type="button" class="btn btn-danger">Delete</button>
     </div>
   </li>`
-    listEl.append(markup)
-
-    $('#taskInput').val('');
-    localStorage.setItem('tasks', task);
-})
-
-/* <li class="list-group-item d-flex justify-content-between align-items-center">
-Sample Task 1
-<div>
-  <button type="button" class="btn btn-warning mr-2">Edit</button>
-  <button type="button" class="btn btn-danger">Delete</button>
-</div>
-</li>*/
+}
